362768c1c432b1c8d8c622cc2c543a36da5a21ae
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-05-07 23:33:16 -0700

    Update app python file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,4 +1,10 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, render_template, request, abort
 
 
 
@@ -8,23 +14,35 @@
 
 
 # reflect an existing database into a new model
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 # reflect the tables
-
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+# Initial parameters to render index.html 
+@app.route("/")
+def index():
+    return render_template("index.html", err_msg="")
+
+
+if __name__ == "__main__":
+    app.run()
